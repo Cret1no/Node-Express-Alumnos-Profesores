bdefa9b7ac749273d1f3a88cf032b003f7dadec8
Emanuel <emanuelalbaitero@gmail.com>
2026-07-02 18:46:27 -0300

    Agregado queryParams(:id) a estudiantesRoutes

--- routes/estudiantesRoutes.js
+++ routes/estudiantesRoutes.js
@@ -9,12 +9,16 @@ router.post('/', (req, res) => {
   res.json({ msg: 'Ingreso de estudiantes' });
 });
 
-router.put('/', (req, res) => {
-  res.json({ msg: 'Actualización de estudiantes' });
-});
-
-router.delete('/', (req, res) => {
-  res.json({ msg: 'Borrado de estudiantes' });
-});
+router
+  .route('/:id')
+  .get((req, res) => {
+    res.json({ msg: 'Consulta de estudiante' });
+  })
+  .put((req, res) => {
+    res.json({ msg: 'Actualización de estudiante' });
+  })
+  .delete((req, res) => {
+    res.json({ msg: 'Borrado de estudiante' });
+  });
 
 module.exports = router;
