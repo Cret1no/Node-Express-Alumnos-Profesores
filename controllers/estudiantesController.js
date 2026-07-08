fe3e19e64469866fac6f33c66418b3bed7f664be
Emanuel <emanuelalbaitero@gmail.com>
2026-07-08 19:27:27 -0300

    Base de datos conectada - estudiantesController(consulta, consultaDetalle e Ingresar funcionando)

--- controllers/estudiantesController.js
+++ controllers/estudiantesController.js
@@ -4,17 +4,50 @@ class EstudiantesController {
   constructor() {}
 
   consultar(req, res) {
-    res.json({ msg: 'Consulta de estudiantes desde clase' });
+    try {
+      db.query('SELECT * FROM estudiantes', (err, rows) => {
+        if (err) {
+          res.status(400).send(err);
+        }
+        res.status(200).json(rows);
+      });
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
   }
 
   consultarDetalle(req, res) {
-    const { id } = req.params;
-    res.json({
-      msg: `Consulta detalle de estudiante desde clase con id:${id}`,
-    });
+    try {
+      const { id } = req.params;
+      db.query(`SELECT * FROM estudiantes WHERE id = ?`, [id], (err, rows) => {
+        if (err) {
+          res.status(400).send(err);
+        }
+        res.status(200).json(rows);
+      });
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
   }
+
   ingresar(req, res) {
-    res.json({ msg: 'Ingreso de estudiante desde clase' });
+    try {
+      const { nombre, apellido, dni, email } = req.body;
+      db.query(
+        `INSERT INTO estudiantes
+        (id, nombre, apellido, dni, email)
+        VALUES(NULL, ?, ?, ?, ?);`,
+        [nombre, apellido, dni, email],
+        (err, rows) => {
+          if (err) {
+            res.status(400).send(err);
+          }
+          res.status(201).json({ id: rows.insertId });
+        },
+      );
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
   }
 
   actualizar(req, res) {
